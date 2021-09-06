cf2b649c4ad99b0e0098988b73b5ffab995113dd
EugeneMaikov <eugene-maikov2000@yandex.ru>
2021-09-06 20:39:07 +0300

    first commi2t

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-
+$(function () {
+  $(".menu__burger-icon").on("click", function () {
+    $(".menu__window").addClass('show');            // Запрет скролить сайт
+  }),
+    $(".close__menu-btn").on("click", function () {
+    $(".menu__window").removeClass('show');            // Запрет скролить сайт
+  })
+})
