47337bcfbb64052affa0754aadde2d215a26c9d0
EugeneMaikov <eugene-maikov2000@yandex.ru>
2021-09-08 13:10:17 +0300

    fffааs

--- js/script.js
+++ js/script.js
@@ -4,5 +4,11 @@ $(function () {
   }),
     $(".close__menu-btn").on("click", function () {
     $(".menu__window").removeClass('show');            // Запрет скролить сайт
+  }),
+    $(".category__burger").on("click", function () {
+    $(".category__window").addClass('show__category');            // Запрет скролить сайт
+  }),
+    $(".close__menu-btn ").on("click", function () {
+    $(".category__window").removeClass('show__category');            // Запрет скролить сайт
   })
 })
